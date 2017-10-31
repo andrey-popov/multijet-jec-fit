9f9f2697af0cef5e5a8f025d6d49857c9f1f465e
Andrey Popov <rainfinder@gmail.com>
2017-10-31 14:17:03 +0100

    Rework command-line arguments
    Allow customization from the command line. It is now possible to choose
    what analyses are to be run by providing coresponding options.
    
    This introduces a dependency on Boost.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ The package is under development.
 Dependencies:
   * CMake 2.8.12 or newer
   * Compiler with support of C++14
+  * Boost 1.34 or newer
   * ROOT 6
 
 To build the package and run an example fit program, execute
@@ -14,6 +15,6 @@ To build the package and run an example fit program, execute
 cmake .
 make
 inputdir="https://aapopov.web.cern.ch/aapopov/jec_inputs/prototype"
-bin/fit $inputdir/photonjet_Run1.root $inputdir/multijet_BinnedSum.root $inputdir/Zjet_Run1.root
-# The input file for the binned sum version of photon+jet is called photonjet_BinnedSum.root
+bin/fit --photonjet-run1 $inputdir/photonjet_Run1.root --multijet-binnedsum $inputdir/multijet_BinnedSum.root --zjet-run1 $inputdir/Zjet_Run1.root
+# An input file for the binned-sum version of the photon+jet analysis is also available and called "photonjet_BinnedSum.root"
 ```
--- prog/fit.cpp
+++ prog/fit.cpp
@@ -1,10 +1,11 @@
 /**
- * Fits jet correction using photon+jet and multijet data.
+ * Fits for jet correction combinning multiple analyses.
  */
 
 #include <JetCorrDefinitions.hpp>
 #include <FitBase.hpp>
 #include <MultijetBinnedSum.hpp>
+#include <PhotonJetBinnedSum.hpp>
 #include <PhotonJetRun1.hpp>
 #include <ZJetRun1.hpp>
 
@@ -12,9 +13,13 @@
 #include <Math/Functor.h>
 #include <TMath.h>
 
+#include <boost/algorithm/string.hpp>
+#include <boost/program_options.hpp>
+
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <memory>
 #include <string>
 
@@ -22,28 +27,78 @@
 int main(int argc, char **argv)
 {
     using namespace std;
+    namespace po = boost::program_options;
     
     
     // Parse arguments
-    if (argc < 4 or argc > 5)
+    po::options_description options("Allowed options");
+    options.add_options()
+      ("help,h", "Prints help message")
+      ("balance,b", po::value<string>()->default_value("PtBal"),
+        "Type of balance variable, PtBal or MPF")
+      ("photonjet-run1", po::value<string>(), "Input file for photon+jet analysis, Run 1 style")
+      ("photonjet-binnedsum", po::value<string>(),
+        "Input file for photon+jet analysis, binned sum")
+      ("zjet-run1", po::value<string>(), "Input file for Z+jet analysis, Run 1 style")
+      ("multijet-binnedsum", po::value<string>(), "Input file for multijet analysis, binned sum")
+      ("output,o", po::value<string>()->default_value("fit.out"),
+        "Name for output file with results of the fit");
+    
+    po::variables_map optionsMap;
+    
+    po::store(
+      po::command_line_parser(argc, argv).options(options).run(),
+      optionsMap);
+    po::notify(optionsMap);
+    
+    if (optionsMap.count("help"))
     {
-        cerr << "Usage: fit inputPhotonJet.root inputMultijet.root inputZJet.root [method]\n";
+        cerr << "Fits for jet correction combinning multiple analyses.\n";
+        cerr << "Usage: fit [options]\n";
+        cerr << options << endl;
         return EXIT_FAILURE;
     }
     
-    bool useMPF = false;
     
-    if (argc >= 5)
+    bool useMPF = false;
+    string balanceVar(optionsMap["balance"].as<string>());
+    boost::to_lower(balanceVar);
+    
+    if (balanceVar == "mpf")
+        useMPF = true;
+    else if (balanceVar != "ptbal")
     {
-        string const methodLabel(argv[4]);
-        
-        if (methodLabel == "MPF")
-            useMPF = true;
-        else if (methodLabel != "PtBal")
-        {
-            cerr << "Do not recognize method \"" << methodLabel << "\".\n";
-            return EXIT_FAILURE;
-        }
+        cerr << "Do not recognize balance variable \"" <<
+          optionsMap["balance"].as<string>() << "\".\n";
+        return EXIT_FAILURE;
+    }
+    
+    
+    // Construct all requested measurements
+    list<unique_ptr<MeasurementBase>> measurements;
+    
+    if (optionsMap.count("photonjet-run1"))
+        measurements.emplace_back(new PhotonJetRun1(optionsMap["photonjet-run1"].as<string>(),
+          (useMPF) ? PhotonJetRun1::Method::MPF : PhotonJetRun1::Method::PtBal));
+    
+    if (optionsMap.count("photonjet-binnedsum"))
+        measurements.emplace_back(new PhotonJetBinnedSum(
+          optionsMap["photonjet-binnedsum"].as<string>(),
+          (useMPF) ? PhotonJetBinnedSum::Method::MPF : PhotonJetBinnedSum::Method::PtBal));
+    
+    if (optionsMap.count("zjet-run1"))
+        measurements.emplace_back(new ZJetRun1(optionsMap["zjet-run1"].as<string>(),
+          (useMPF) ? ZJetRun1::Method::MPF : ZJetRun1::Method::PtBal));
+    
+    if (optionsMap.count("multijet-binnedsum"))
+        measurements.emplace_back(new MultijetBinnedSum(
+          optionsMap["multijet-binnedsum"].as<string>(),
+          (useMPF) ? MultijetBinnedSum::Method::MPF : MultijetBinnedSum::Method::PtBal));
+    
+    if (measurements.empty())
+    {
+        cerr << "No measurements requested.\n";
+        return EXIT_FAILURE;
     }
     
     
@@ -51,19 +106,11 @@ int main(int argc, char **argv)
     auto jetCorr = make_unique<JetCorrStd2P>();
     CombLossFunction lossFunc(move(jetCorr));
     
-    PhotonJetRun1 measurementPhotonJet(argv[1],
-      (useMPF) ? PhotonJetRun1::Method::MPF : PhotonJetRun1::Method::PtBal);
-    lossFunc.AddMeasurement(&measurementPhotonJet);
-    
-    ZJetRun1 measurementZJet(argv[3],
-      (useMPF) ? ZJetRun1::Method::MPF : ZJetRun1::Method::PtBal);
-    lossFunc.AddMeasurement(&measurementZJet);
-    
-    MultijetBinnedSum measurementMultijet(argv[2],
-      (useMPF) ? MultijetBinnedSum::Method::MPF : MultijetBinnedSum::Method::PtBal);
-    lossFunc.AddMeasurement(&measurementMultijet);
+    for (auto const &measurement: measurements)
+        lossFunc.AddMeasurement(measurement.get());
     
     unsigned const nPars = lossFunc.GetNumParams();
+    
     
     // Create minimizer
     ROOT::Minuit2::Minuit2Minimizer minimizer;
@@ -103,7 +150,7 @@ int main(int argc, char **argv)
     
     
     // Save fit results in a text file
-    string const resFileName("fit.out");
+    string const resFileName(optionsMap["output"].as<string>());
     ofstream resFile(resFileName);
     
     resFile << "# Fitted parameters\n";
